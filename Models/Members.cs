bc0c3f727bb25e6cc6b5fca997ad2ddc5a5c1078
Jorich P <w3bslave@gmail.com>
2022-10-30 16:42:25 -0400

    Add project files.

---
+++ Models/Members.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Member
+{
+    public string MemID { get; set; }
+    public string MemName { get; set; }
+    public string MemRole { get; set; }
+    public string MemContribute { get; set; }    
+    public Member()
+    {
+
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace GroupK_A3.Models
+{
+    public class Reservation
+    {
+        //Personal Details
+        public int Id { get; set; } 
+
+        [Display(Name = "Full Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Required]
+        public string Phone { get; set; }
+
+        [Display(Name = "Home Address")]
+        [Required]
+        public string HomeAddress { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //Booking Details
+        [Display(Name = "Room Type")]
+        [Required]
+        public DateTime RoomType { get; set; }
+
+        [Display(Name = "Total Person Staying")]
+        [Required]
+        public int TotalPerson { get; set; }
+
+        [Display(Name = "Check-in Date")]
+        [Required]
+        public DateTime CheckinDateTime { get; set; }
+
+        [Display(Name = "Check-out Date")]
+        [Required]
+        public DateTime CheckoutDateTime { get; set; }
+
+        [NotMapped]
+        public string CheckinDate => CheckinDateTime.ToString("MM/dd/yyyy");
+
+        [NotMapped]
+        public string CheckinTime => CheckoutDateTime.ToString("hh:mm tt");
+
+        [NotMapped]
+        public string CheckoutDate => CheckoutDateTime.ToString("MM/dd/yyyy");
+
+        [NotMapped]
+        public string CheckouTime => CheckoutDateTime.ToString("hh:mm tt");
+
+        public string SpecialRequest { get; set; }
+
+
+        public Reservation()
+        {
+
+        }
+
+    }
+}
